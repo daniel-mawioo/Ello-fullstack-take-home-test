f21d28689cf905e0ee1a6a33245506104921293a
daniel-mawioo <mawioodaniel43@gmail.com>
2024-06-10 08:50:12 +0300

    fe

--- frontend-ello/src/apollo/client.js
+++ frontend-ello/src/apollo/client.js
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/", // Ensure this matches your backend server URL
+  uri: "https://ello-fullstack-take-home-test.onrender.com/graphql",
   cache: new InMemoryCache(),
 });
 
